a7c2df87f855a860d0ba3733f6918cb3231ca003
andymina <andy.mina@macaulay.cuny.edu>
2021-04-27 15:36:14 -0400

    Initial Commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by Andy Mina on 4/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
